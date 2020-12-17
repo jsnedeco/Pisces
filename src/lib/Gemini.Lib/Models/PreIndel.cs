044b6b1c3e6889b3a4310e37449670de4ffa7e8e
tamsen <tamsen.dunn@gmail.com>
2020-12-16 16:14:28 -0800

    it looks like there is a deployment issue where Gemini can't be both a library and an exe at the same time. Work around is to split out a library.

---
+++ src/lib/Gemini.Lib/Models/PreIndel.cs
@@ -0,0 +1,23 @@
+﻿using Pisces.Domain.Models.Alleles;
+
+namespace Gemini.Models
+{
+    public class PreIndel : CandidateAllele
+    {
+        public int LeftAnchor;
+        public int RightAnchor;
+        public int Mess;
+        public int Score;
+        public int AverageQualityRounded;
+        public bool InMulti;
+        public string OtherIndel;
+        public int Observations;
+
+        public PreIndel(CandidateAllele baseCandidateAllele)
+            : base(baseCandidateAllele.Chromosome, baseCandidateAllele.ReferencePosition, baseCandidateAllele.ReferenceAllele, baseCandidateAllele.AlternateAllele, baseCandidateAllele.Type)
+        {
+            SupportByDirection = baseCandidateAllele.SupportByDirection;
+            IsKnown = baseCandidateAllele.IsKnown;
+        }
+    }
+}
