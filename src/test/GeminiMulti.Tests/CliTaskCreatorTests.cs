c3df3afca455bd968e5ba3aa62c5c2bd9b01cab6
tamsen <tamsen.dunn@gmail.com>
2020-12-16 17:47:05 -0800

    gemini seems to be working in "not multiprocess" mode. Need to debug the multiprocessor...

--- src/test/GeminiMulti.Tests/CliTaskCreatorTests.cs
+++ src/test/GeminiMulti.Tests/CliTaskCreatorTests.cs
@@ -13,7 +13,9 @@ namespace GeminiMulti.Tests
                 "Outdir", 1);
 
             string OS_SpecificString=Path.Combine("path", "with spaces","myexe");
-            Assert.Equal( "\"" + OS_SpecificString + "\" --args1 1thing -args2 another --chromRefId \"1\" --outFolder \"Outdir\"", task.CommandLineArguments);
+            string expected_string =  "\"" + OS_SpecificString + "\" --args1 1thing -args2 another --chromRefId \"1\" --outFolder \"Outdir\"";
+            string actual_string = task.CommandLineArguments;
+            Assert.Equal(expected_string,actual_string );    
         }
     }
 }
